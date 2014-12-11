5e2babaab32ad7a99b42c91c7c03d30677c07fcc
Nidal <nidal@level09.com>
2014-12-11 17:50:26 +0400

    fixed host url

--- public/views.py
+++ public/views.py
@@ -9,7 +9,7 @@ bp_public = Blueprint('public',__name__, static_folder='../static')
 
 @bp_public.route('/')
 def index():
-    return Response('{"API": {"status":"ok","version":"1.0"}}', content_type ='application/json')
+    return Response('{"API": {"status":"ok","version":"1.0"}}', mimetype ='application/json')
 
 @bp_public.route('/v1/url',methods=['GET','POST'])
 def process():
@@ -20,29 +20,29 @@ def process():
     if longUrl:
         return shorten(longUrl)
     else:
-        return Response('{"error": {"code":400,"message":"API Error"}}',content_type='application/json')
+        return Response('{"error": {"code":400,"message":"API Error"}}',mimetype='application/json')
 
 
 def shorten(longUrl):
     try:
         link = Link.objects.get(longUrl=longUrl)
-        return Response('{"id":"%s","longUrl":"%s"}' % (request.host_url + link.shortUrl,link.longUrl),content_type='application/json')
+        return Response('{"id":"%s","longUrl":"%s"}' % (request.host_url + link.shortUrl,link.longUrl),mimetype='application/json')
     except Exception, e:
         #first save it to get the id
         link = Link(longUrl=longUrl)
         link.save()
         link.shortUrl = short_url.encode_url(link.lid)
         link.save()
-        shortUrl =  request.host + link.shortUrl
-        return Response('{"id":"%s","longUrl":"%s"}' % (shortUrl,longUrl),content_type='application/json',status=200)
+        shortUrl =  (%s/%s) % (request.host,link.shortUrl)
+        return Response('{"id":"%s","longUrl":"%s"}' % (shortUrl,longUrl),mimetype='application/json')
 
 def expand(shortUrl):
     key = short_url.decode_url(shortUrl.split('/')[-1])
     try:
         link = Link.objects.get(lid=key)
-        return Response('{"id":"%s","longUrl":"%s","status":"ok"}' % (shortUrl,link.longUrl),content_type='application/json')
+        return Response('{"id":"%s","longUrl":"%s","status":"ok"}' % (shortUrl,link.longUrl),mimetype='application/json')
     except Exception:
-        return Response('{"error": {"code":400,"message":"API Error"}}',content_type='application/json')
+        return Response('{"error": {"code":400,"message":"API Error"}}',mimetype='application/json')
 
 @bp_public.route('/<shortUrl>')
 def redir(shortUrl=None):
@@ -54,4 +54,4 @@ def redir(shortUrl=None):
         print link.longUrl
         return redirect(link.longUrl,301)
     except Exception:
-        return Response('{"error": {"code":400,"message":"Key Error"}}',content_type='application/json')
+        return Response('{"error": {"code":400,"message":"Key Error"}}',mimetype='application/json')
